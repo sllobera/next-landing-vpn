f082829173aef728e4ba2d87dc719fcbd0cd7689
sllobera <sebastian@llobera.com.au>
2024-10-03 16:04:15 +1000

    pp

--- components/Feature.js
+++ components/Feature.js
@@ -5,11 +5,16 @@ import getScrollAnimation from "../utils/getScrollAnimation";
 import ScrollAnimationWrapper from "./Layout/ScrollAnimationWrapper";
 
 const features = [
-  "Powerfull online protection.",
-  "Internet without borders.",
-  "Supercharged VPN",
-  "No specific time limits."
-]
+  "Save up to 70% on staff costs",
+  "High standard recruitment",
+  "Flexible contracts",
+  "Finance & Accounting",
+  "Digital Marketing",
+  "Information Technology",
+  "Administrative Support",
+  "Customer Service",
+  "Creative Design",
+];
 
 const Feature = () => {
   const scrollAnimation = useMemo(() => getScrollAnimation(), []);
@@ -23,44 +28,47 @@ const Feature = () => {
         <ScrollAnimationWrapper className="flex w-full justify-end">
           <motion.div className="h-full w-full p-4" variants={scrollAnimation}>
             <Image
-              src="/assets/Illustration2.png"
-              alt="VPN Illustrasi"
+              src="/assets/hanhngo.webp"
+              alt="Hahn Ngo"
               layout="responsive"
               quality={100}
-              height={414}
-              width={508}
+              height={314}
+              width={218}
             />
           </motion.div>
         </ScrollAnimationWrapper>
         <ScrollAnimationWrapper>
-
-        <motion.div className="flex flex-col items-end justify-center ml-auto w-full lg:w-9/12" variants={scrollAnimation}>
-          <h3 className="text-3xl lg:text-4xl font-medium leading-relaxed text-black-600">
-            We Provide Many Features You Can Use
-          </h3>
-          <p className="my-2 text-black-500">
-            You can explore the features that we provide with fun and have their
-            own functions each feature.
-          </p>
-          <ul className="text-black-500 self-start list-inside ml-8">
-            {features.map((feature, index) => (
-              <motion.li
-                className="relative circle-check custom-list"
-                custom={{duration: 2 + index}}
-                variants={scrollAnimation}
-                key={feature}
-                whileHover={{
-                scale : 1.1,
-                transition: {
-                  duration: .2
-                }
-                }}>
+          <motion.div
+            className="flex flex-col items-end justify-center ml-auto w-full lg:w-9/12"
+            variants={scrollAnimation}
+          >
+            <h3 className="text-3xl lg:text-4xl font-medium leading-relaxed text-black-600">
+              Feeling Overwhelmed by Outsourcing?
+            </h3>
+            <p className="my-2 text-black-500">
+              Why trust us? Because we make outsourcing simple, clear, and
+              effective. Let us handle the complexity so you can focus on what
+              matters most – growing your business.
+            </p>
+            <ul className="text-black-500 self-start list-inside ml-8">
+              {features.map((feature, index) => (
+                <motion.li
+                  className="relative circle-check custom-list"
+                  custom={{ duration: 2 + index }}
+                  variants={scrollAnimation}
+                  key={feature}
+                  whileHover={{
+                    scale: 1.1,
+                    transition: {
+                      duration: 0.2,
+                    },
+                  }}
+                >
                   {feature}
-              </motion.li>
-              )
-            )}
-          </ul>
-        </motion.div>
+                </motion.li>
+              ))}
+            </ul>
+          </motion.div>
         </ScrollAnimationWrapper>
       </div>
     </div>
--- components/Hero.js
+++ components/Hero.js
@@ -1,11 +1,11 @@
 import React, { useMemo } from "react";
 import Image from "next/image";
 import ButtonPrimary from "./misc/ButtonPrimary";
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 import getScrollAnimation from "../utils/getScrollAnimation";
 import ScrollAnimationWrapper from "./Layout/ScrollAnimationWrapper";
 
-const Hero = ({ 
+const Hero = ({
   listUser = [
     {
       name: "Projects",
@@ -27,45 +27,43 @@ const Hero = ({
   const scrollAnimation = useMemo(() => getScrollAnimation(), []);
 
   return (
-    <div
-      className="max-w-screen-xl mt-24 px-8 xl:px-16 mx-auto"
-      id="about"
-    >
+    <div className="max-w-screen-xl mt-24 px-8 xl:px-16 mx-auto" id="about">
       <ScrollAnimationWrapper>
-          <motion.div
-            className="grid grid-flow-row sm:grid-flow-col grid-rows-2 md:grid-rows-1 sm:grid-cols-2 gap-8 py-6 sm:py-16"
-            variants={scrollAnimation}>
-            <div className=" flex flex-col justify-center items-start row-start-2 sm:row-start-1">
-              <h1 className="text-3xl lg:text-4xl xl:text-5xl font-medium text-black-600 leading-normal">
+        <motion.div
+          className="grid grid-flow-row sm:grid-flow-col grid-rows-2 md:grid-rows-1 sm:grid-cols-2 gap-8 py-6 sm:py-16"
+          variants={scrollAnimation}
+        >
+          <div className=" flex flex-col justify-center items-start row-start-2 sm:row-start-1">
+            <h1 className="text-3xl lg:text-4xl xl:text-5xl font-medium text-black-600 leading-normal">
               Compare <strong> Outsourcing</strong>.
-              </h1>
-              <p className="text-black-500 mt-4 mb-6">
-              We‘ve been saving Aussies time, effort and money comparing a range of Australia‘s Outsourcing Companies for over 20 years
-              </p>
-              <ButtonPrimary>Get Started</ButtonPrimary>
-            </div>
-            <div className="flex w-full">
-              <motion.div className="h-full w-full" variants={scrollAnimation}>
-                <Image
-                  src="/assets/"
-                  alt="VPN Illustrasi"
-                  quality={100}
-                  width={612}
-                  height={383}
-                  layout="responsive"
-                />
-              </motion.div>
-            </div>
-          </motion.div>
+            </h1>
+            <p className="text-black-500 mt-4 mb-6">
+              We‘ve been saving Aussies time, effort and money comparing a range
+              of Australia‘s Outsourcing Companies for over 20 years
+            </p>
+            <ButtonPrimary>Get Started</ButtonPrimary>
+          </div>
+          <div className="flex w-full">
+            <motion.div className="h-full w-full" variants={scrollAnimation}>
+              <Image
+                src="/assets/team.png"
+                alt="Expert Outsourcing Team "
+                quality={100}
+                width={612}
+                height={383}
+                layout="responsive"
+              />
+            </motion.div>
+          </div>
+        </motion.div>
       </ScrollAnimationWrapper>
       <div className="relative w-full flex">
-        <ScrollAnimationWrapper
-          className="rounded-lg w-full grid grid-flow-row sm:grid-flow-row grid-cols-1 sm:grid-cols-3 py-9 divide-y-2 sm:divide-y-0 sm:divide-x-2 divide-gray-100 bg-white-500 z-10">
+        <ScrollAnimationWrapper className="rounded-lg w-full grid grid-flow-row sm:grid-flow-row grid-cols-1 sm:grid-cols-3 py-9 divide-y-2 sm:divide-y-0 sm:divide-x-2 divide-gray-100 bg-white-500 z-10">
           {listUser.map((listUsers, index) => (
             <motion.div
               className="flex items-center justify-start sm:justify-center py-4 sm:py-6 w-8/12 px-4 sm:w-auto mx-auto sm:mx-0"
               key={index}
-              custom={{duration: 2 + index}}
+              custom={{ duration: 2 + index }}
               variants={scrollAnimation}
             >
               <div className="flex mx-auto w-40 sm:w-auto">
@@ -81,11 +79,11 @@ const Hero = ({
               </div>
             </motion.div>
           ))}
-       </ScrollAnimationWrapper>
-       <div
+        </ScrollAnimationWrapper>
+        <div
           className="absolute bg-black-600 opacity-5 w-11/12 roudned-lg h-64 sm:h-48 top-0 mt-8 mx-auto left-0 right-0"
           style={{ filter: "blur(114px)" }}
-       ></div>
+        ></div>
       </div>
     </div>
   );
