88b3fff34ea341d6ec120312d8e4b4ff5ad0e51f
sllobera <sebastian@llobera.com.au>
2024-10-03 16:49:17 +1000

    almost

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -62,7 +62,7 @@ const Header = () => {
             >
               Feature
             </LinkScroll>
-             
+
             <LinkScroll
               activeClass="active"
               to="testimoni"
@@ -83,12 +83,7 @@ const Header = () => {
             </LinkScroll>
           </ul>
           <div className="col-start-10 col-end-12 font-medium flex justify-end items-center">
-            <Link href="/">
-              <a className="text-black-600 mx-2 sm:mx-4 capitalize tracking-wide hover:text-orange-500 transition-all">
-                 
-              </a>
-            </Link>
-            <ButtonOutline>Book a call</ButtonOutline> 
+            <ButtonOutline>Book a call</ButtonOutline>{" "}
           </div>
         </nav>
       </header>
@@ -161,38 +156,7 @@ const Header = () => {
               </svg>
               Feature
             </LinkScroll>
-            <LinkScroll
-              activeClass="active"
-              to="pricing"
-              spy={true}
-              smooth={true}
-              duration={1000}
-              onSetActive={() => {
-                setActiveLink("pricing");
-              }}
-              className={
-                "mx-1 sm:mx-2 px-3 sm:px-4 py-2 flex flex-col items-center text-xs border-t-2 transition-all " +
-                (activeLink === "pricing"
-                  ? "  border-orange-500 text-orange-500"
-                  : " border-transparent ")
-              }
-            >
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-              Pricing
-            </LinkScroll>
+
             <LinkScroll
               activeClass="active"
               to="testimoni"
--- components/misc/ButtonOutline..js
+++ components/misc/ButtonOutline..js
@@ -1,10 +1,12 @@
 import React from "react";
-
+import Link from "next/link";
 const ButtonOutline = ({ children }) => {
   return (
     <button className="font-medium tracking-wide py-2 px-5 sm:px-8 border border-orange-500 text-orange-500 bg-white-500 outline-none rounded-l-full rounded-r-full capitalize hover:bg-orange-500 hover:text-white-500 transition-all hover:shadow-orange ">
       {" "}
-      {children}
+      <Link href="https://calendly.com/seb-py-h/30min?month=2024-10">
+        {children}
+      </Link>
     </button>
   );
 };
